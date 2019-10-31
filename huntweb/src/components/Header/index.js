d254702911f6a0e52cba4ac225cd3c28d6e54ac6
Larissa <lariellen67@gmail.com>
2019-10-31 17:00:25 -0300

    up

---
+++ huntweb/src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./styles.css";
+
+const Header = () => 
+    <header id="main-header">BEYONCÉ RULES</header>
+
+
+export default Header;
+
+
+
+/*class Header extends Component { // ambas as funções são equivalentes, tanto faz usar uma ou outra
+    render(){
+        return <h1>Driver roll up the partion, please</h1>
+    }
+
+}*/
+
