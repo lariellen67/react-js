c37a2fa4277992d519da6393ba07a31e43b23461
Larissa <lariellen67@gmail.com>
2019-11-01 14:45:35 -0300

    'up'

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 export default class Main extends Component{
     state = {
-        products: []
+        products: [],
+        productInfo: {},
+        page: 1,
     }
 
 
@@ -12,26 +15,43 @@ export default class Main extends Component{
         this.loadProducts();
     }
 
-    loadProducts = async () =>{
-        const response = await api.get("/products");
+    loadProducts = async (page = 1) =>{
+        const response = await api.get(`/products?pages=${page}`);
 
-        this.setState({ products: response.data.docs });
+        const { docs, ...productInfo } = response.data;
+
+        this.setState({ products: docs, productInfo, page });
     };
 
+    prevPage = () => {
+        const {page, productInfo} = this.state;
+        if (page === 1) return;
+        const pageNumber = page - 1;
+        this.loadProducts(pageNumber);
+    };
+
+    nextPage = () => {
+        const {page, productInfo} = this.state;
+        if (page === productInfo.pages) return;
+        const pageNumber = page + 1;
+        this.loadProducts(pageNumber);
+    };
+
+
     render(){
-        const {products} = this.state;
+        const {products, page, productInfo} = this.state;
         return (
         <div className="product-list">
             {products.map(product => (
                 <article key={product._id}>
                     <strong>{product.title}</strong>
                     <p>{product.description}</p>
-                    <a href="">Acessar</a>
+                    <Link to={`/products/${product._id}`}>Clica aqui e tell boy bye</Link>
                 </article>
             ))}
             <div className="actions">
-                <button>Anterior</button>
-                <button>Próxima</button>
+                <button disabled={page ===1} onClick={this.prevPage}>Anterior</button>
+                <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
             </div>
         </div>
         );
